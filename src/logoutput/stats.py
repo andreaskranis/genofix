b45f5f1e2c77a7c25ddd3c5c69dc7e2f8214fb08
hindlem <matthew.hindle@gmail.com>
2021-09-22 11:22:21 +0100

    compress logged output

--- src/logoutput/stats.py
+++ src/logoutput/stats.py
@@ -4,14 +4,15 @@ Created on Sep 21, 2021
 @author: mhindle
 '''
 import os
+import gzip
 
 class Stats(object):
 
     def __init__(self, outputdir=None):
         self.outputdir = outputdir if outputdir is not None else os.getcwd()
         os.makedirs(self.outputdir, exist_ok=True)
-        self.pair_stats = open("%s/pair_stats.txt" % self.outputdir, "wt")
-        self.single_stats = open("%s/single_stats.txt" % self.outputdir, "wt")
+        self.pair_stats = gzip.open("%s/pair_stats.txt.gz" % self.outputdir, "wt")
+        self.single_stats = gzip.open("%s/single_stats.txt.gz" % self.outputdir, "wt")
     
     def __enter__(self):
         return self
