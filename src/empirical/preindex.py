60d65ef8fa40aec1917dc265fa2add3e026d1a2a
mhindle <mhindle@NB-RND-MH.uk.itd>
2022-07-28 12:48:23 +0100

    revert to c engine

--- src/empirical/preindex.py
+++ src/empirical/preindex.py
@@ -159,10 +159,10 @@ USAGE
         datatypes = {snp:np.uint8 for snp in snps} | {"id":np.int64}
         if genotypes_input_file.endswith(".gz") :
             genotypes = pd.read_csv(genotypes_input_file, usecols=filtercolumns,
-                                    sep=" ", compression='gzip', header=0, index_col=0, engine="pyarrow", dtype=datatypes)
+                                    sep=" ", compression='gzip', header=0, index_col=0, engine="c", dtype={snp:np.uint8 for snp in snps}, low_memory=False, memory_map=True)
         else :
             genotypes = pd.read_csv(genotypes_input_file, usecols=filtercolumns,
-                                     sep=" ", header=0, index_col=0, engine="pyarrow", dtype=datatypes)
+                                     sep=" ", header=0, index_col=0, engine="c", dtype=, low_memory=False, memory_map=True)
         print("Loaded genotype matrix with %s individuals X %s snps " %genotypes.shape)
         
         candidatesForEval = list()
